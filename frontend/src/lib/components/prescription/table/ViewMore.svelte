15048194a3f75f5cc5a36e51adfa45e6dd21e0af
tommylay1902 <tommylay.c@gmail.com>
2025-04-27 12:14:09 -0700

    started prescription details

---
+++ frontend/src/lib/components/prescription/table/ViewMore.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import EllipsisVertical from '@lucide/svelte/icons/ellipsis-vertical';
+	import * as Tooltip from '$lib/components/ui/tooltip/index.js';
+	let { pId } = $props();
+</script>
+
+<div>
+	<Tooltip.Provider>
+		<Tooltip.Root>
+			<Tooltip.Trigger>
+				<a href={`/user/prescriptions/${pId}`} class="hover:bg-gray-500">
+					<EllipsisVertical />
+				</a>
+			</Tooltip.Trigger>
+			<Tooltip.Content>Click to view more details of the given prescription</Tooltip.Content>
+		</Tooltip.Root>
+	</Tooltip.Provider>
+</div>
